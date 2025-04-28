74c57e61ff5dc65bb561248bb495e2919e49468e
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-04-28 12:06:09 -0700

    Clean up

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.cat import Cat
 from ..db import db
-# from ..models.cat import cats
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix = "/cats")
 
@@ -95,42 +94,3 @@ def delete_cat(id):
 
 
 
-# @cats_bp.get("")
-# def get_all_cats():
-#     results_list = []
-
-#     for cat in cats:
-#         results_list.append(dict(
-#             id = cat.id,
-#             name = cat.name,
-#             color = cat.color,
-#             personality = cat.personality
-#         ))
-#     return results_list
-
-
-# @cats_bp.get("/<id>")
-# def get_one_cat(id):
-#     cat = validate_cat(id)
-#     cat_dict = dict(
-#         id = cat.id,
-#         name = cat.name,
-#         color= cat.color,
-#         personality = cat.personality
-#     )
-    
-#     return cat_dict
-
-# def validate_cat(id):
-#     try:
-#         id = int(id)
-#     except ValueError:
-#         invalid = {"message": f"Cat id ({id}) is invalid."}
-#         abort(make_response(invalid, 400))
-
-#     for cat in cats:
-#         if cat.id == id:
-#             return cat 
-    
-#     not_found = {"message": f"Cat with id ({id}) not found."}
-#     abort(make_response(not_found, 404))
